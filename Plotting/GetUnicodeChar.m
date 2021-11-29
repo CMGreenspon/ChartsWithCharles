787e1100bc768e3e3026f36e71f202b56cbc4d96
CMGreenspon <charles.greenspon@gmail.com>
2021-11-29 15:54:00 -0600

    .

--- Plotting/GetUnicodeChar.m
+++ Plotting/GetUnicodeChar.m
@@ -1,11 +1,11 @@
-function gc = GetUnicodeChar(character)
+function uc = GetUnicodeChar(character)
 % Get the unicode char value for a unicode character
-% gc = GetGreekChar('letter' [string])
-% The 'letter' requested is case sensitive. 'Gamma' ~= 'gamma'
-% If 'all' is passed then a structure containing all letters will be generated.
+% gc = GetUnicodeChar('character' [string])
+% The 'character' requested is case sensitive. 'Gamma' ~= 'gamma'
+% If 'all' is passed then a structure containing all character will be generated.
 
 % See https://www.webstandards.org/learn/reference/charts/entities/symbol_entities/index.html for other characters
-% Be aware that not all typefaces support Greek letters. I suggest OpenSans or RobotoMono (can be found on google fonts)
+% Be aware that not all typefaces support unicode characters. I suggest OpenSans or RobotoMono (can be found on google fonts)
 
     names = {'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa','Lambda',...
              'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega',...
@@ -13,8 +13,8 @@ function gc = GetUnicodeChar(character)
              'mu', 'nu', 'xi', 'omicron', 'pi', 'rho', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega'};
     unicode_values = [913:929, 931:937, 945:961, 963:969];
     
-    if strcmpi(character, 'all') % Generate struct with all letters
-        gc = struct();
+    if strcmpi(character, 'all') % Generate struct with all characters
+        uc = struct();
         for i = 1:length(names)
             eval(['gc.', names{i}, ' = char(', num2str(unicode_values(i)), ');'])
         end
@@ -22,10 +22,10 @@ function gc = GetUnicodeChar(character)
     else % Find the specific letter and only give that one
         letter_idx = find(strcmp(names, character));
         if length(letter_idx) ~= 1
-            error(sprintf('Could not find letter: "%s"', character))
+            error(sprintf('Could not find character: "%s"', character))
             return
         else
-            gc = char(unicode_values(letter_idx));
+            uc = char(unicode_values(letter_idx));
         end
     
     end
