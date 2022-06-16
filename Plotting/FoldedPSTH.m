a815a5b44347b189356bd61e109c39896aef13a8
CMGreenspon <charles.greenspon@gmail.com>
2022-06-16 09:09:06 -0500

    .

--- Plotting/FoldedPSTH.m
+++ Plotting/FoldedPSTH.m
@@ -18,7 +18,7 @@ function folded_hist = FoldedPSTH(spike_times, bin_edges, n_folds, gauss_width)
     folded_hist = zeros([n_folds, length(bin_edges)-1]);
     
     for f = 1:n_folds
-        temp_spikes = horzcat(spike_times{1,fold_idx(f,:)});
+        temp_spikes = horzcat(spike_times{fold_idx(f,:)});
         temp_hist = histcounts(temp_spikes, bin_edges) / range(bin_edges(1:2));
         temp_hist = smoothdata(temp_hist ./ trials_per_fold, 'gaussian', gauss_width);
         folded_hist(f,:) = temp_hist;
--- Plotting/Rasterfy.m
+++ Plotting/Rasterfy.m
@@ -3,19 +3,20 @@ function raster_ticks = Rasterfy(spike_times, y_margin)
         y_margin = 0.4;
     end
     num_trials = length(spike_times);
-        
+    
+    raster_ticks = cell([num_trials,2]);
     for t = 1:num_trials
         [x_vec, y_vec] = deal(NaN(1,1));
 
-        trial_spikes = spike_times{1,t};
+        trial_spikes = spike_times{t};
         num_trial_spikes = length(trial_spikes);
         for s = 1:num_trial_spikes
-            x_vec = [x_vec, trial_spikes(s), trial_spikes(s), NaN];
+            x_vec = [x_vec, trial_spikes(s), trial_spikes(s), NaN]; %#ok<*AGROW> 
             y_vec = [y_vec, -y_margin, y_margin, NaN];
         end
         
         raster_ticks{t,1} = x_vec;
-        raster_ticks{t,2} = y_vec;
+        raster_ticks{t,2} = y_vec + t;
     end
     
 end  
