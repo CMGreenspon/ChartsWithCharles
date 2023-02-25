0ab1a76171398f81575ce4a60790c69347fa4eae
Charles M. Greenspon <charles.greenspon@gmail.com>
2023-02-25 09:12:27 -0600

    p > 1

--- Miscellaneous/pStr.m
+++ Miscellaneous/pStr.m
@@ -5,6 +5,8 @@ function p_str = pStr(p, n)
     
     if p < 10^-n
         p_str = ['p < 0.', repmat('0', 1,n-1), '1'];
+    elseif p >= 1
+        p_str = ['p = 1.', repmat('0', 1,n)];
     else
         p_str = sprintf(['p = %0.', num2str(n), 'f'], p);
     end
